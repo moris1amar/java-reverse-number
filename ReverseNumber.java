799b758353261c234df2fe58258a8da393dc9184
Maurice <moris3amar@gmail.com>
2017-01-16 22:58:47 +0200

    Ajout du fichier ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+public class ReverseNumber{
+	public static void main(String[] args) {
+		int number = 1234;
+		int reversedNumber = 0 ;
+		int temp = 0;
+
+		while (number) > 0 {
+			temp = number%10;
+
+			reverseNumber = reverseNumber * 10 + temp;
+			number = number/10;
+		}
+
+		System.out.println("Reversed Number is: " + reversedNumber);
+	}
+}
+
